960d2248c7c67d6cf2f089b0155ee404a2bb5184
dmfenton <dfenton@esri.com>
2018-05-22 08:42:39 -0400

    add simple bench

---
+++ bench.js
@@ -0,0 +1,14 @@
+const Benchmark = require('benchmark');
+const fs = require('fs')
+const Sapling = require('./')
+const featureCollection = require('./test/fixtures/featureCollection.json')
+
+const bench = new Benchmark('Two Feature', function () {
+  const sapling = new Sapling()
+  sapling.addFeatures(featureCollection.features)
+  return sapling.createDom()
+})
+
+bench.run()
+
+console.log(bench.stats)
